aa8e0dbba67b9cf5057f24ced0a07dde891ea8eb
AKRAMEE <nebihakram@users.noreply.github.com>
2021-01-05 17:24:49 +0100

    first commit

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public GameObject character;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position=new Vector3(character.transform.position.x+ 5.5f,0,-10);
+    }
+}
---
+++ Assets/CharacterBehavior.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterBehavior : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float vitesse;
+    public float maxjump;
+    private bool isGrounded=false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity += new Vector2 (vitesse,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("space") && isGrounded){
+            jump();
+        }
+    }
+    void jump(){
+            rb.velocity += new Vector2 (0,maxjump);
+    }
+   void OnCollisionEnter2D(Collision2D col){
+        if(col.gameObject.CompareTag("Ground")){
+            isGrounded=true;
+        }
+
+    }
+    void OnCollisionExit2D(Collision2D col){
+         if(col.gameObject.CompareTag("Ground")){
+            isGrounded=false;
+        }
+
+    }
+}
